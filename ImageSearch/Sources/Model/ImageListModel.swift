df32deac64b1e5a143924ec8d82838ded4fa9ce8
s-doris <swdoriz@gmail.com>
2021-06-21 23:33:23 +0900

    feat : ImageListModel init

---
+++ ImageSearch/Sources/Model/ImageListModel.swift
@@ -0,0 +1,31 @@
+//
+//  ImageListModel.swift
+//  ImageSearch
+//
+//  Created by once on 2021/06/21.
+//
+import Foundation
+import RxSwift
+
+protocol ImageListFetchable: AnyObject {
+  func fetchImageList(param: String) -> Observable<[Image]>
+}
+
+class ImageListModel: ImageListFetchable {
+  let repository: NetworkServiceType
+  
+  init(repository: NetworkServiceType = NetworkService()) {
+    self.repository = repository
+  }
+  
+  func fetchImageList(param: String) -> Observable<[Image]> {
+    return self.repository.getImageListRx(param: param)
+      .map { data in
+        guard let response = try? JSONDecoder().decode([Image].self, from: data) else {
+          throw NSError(domain: "Decoding error", code: -1, userInfo: nil)
+        }
+        
+        return response
+      }
+  }
+}

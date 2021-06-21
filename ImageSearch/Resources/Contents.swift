a1067211fba59d45904cc24f9d285e41d522a7fa
s-doris <swdoriz@gmail.com>
2021-06-22 01:36:41 +0900

    fix : BASE_URL typo 수정

--- ImageSearch/Resources/Contents.swift
+++ ImageSearch/Resources/Contents.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 enum API {
-  static let BASE_URL: String = "http://dapi.kakao.com/v2/"
+  static let BASE_URL: String = "https://dapi.kakao.com/v2/"
   static let KEY: String = "KakaoAK 4a27664742cb0f6fdab1e4235df73921"
 }

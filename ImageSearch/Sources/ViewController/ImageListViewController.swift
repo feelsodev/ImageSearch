71bee1ccaac4cba20f2307825b85ed7e1950993c
s-doris <swdoriz@gmail.com>
2021-06-22 01:04:56 +0900

    feat : ImageListViewController searchBar Rx 바인딩 적용

--- ImageSearch/Sources/ViewController/ImageListViewController.swift
+++ ImageSearch/Sources/ViewController/ImageListViewController.swift
@@ -6,15 +6,25 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
 
 class ImageListViewController: BaseViewController {
+  
+  
   
   // MARK: - Constants
   
   struct Metric {
-    static let width = UIScreen.main.bounds.width
-    static let height = UIScreen.main.bounds.height
+    static let cellWidth = UIScreen.main.bounds.width / 3
+    static let CellHeight = UIScreen.main.bounds.height / 3
   }
+  
+  
+  // MARK: - Properties
+  
+  var viewModel: ImageListViewModelType
   
   
   // MARK: - UI
@@ -23,25 +33,59 @@ class ImageListViewController: BaseViewController {
   let imageListView = UICollectionView(
     frame: .zero,
     collectionViewLayout: UICollectionViewFlowLayout().then {
-      $0.minimumLineSpacing = 2
-      $0.minimumInteritemSpacing = 2
-      $0.scrollDirection = .horizontal
-      $0.itemSize = CGSize(width: Metric.width / 3 - 8,
-                           height: Metric.width / 3 - 8)
+      $0.minimumLineSpacing = .zero
+      $0.minimumInteritemSpacing = .zero
+      $0.scrollDirection = .vertical
+      $0.itemSize = CGSize(width: Metric.cellWidth,
+                           height: Metric.CellHeight)
     }
   ).then {
     $0.backgroundColor = .blue
-    $0.register(ImageListCell.self,
-                forCellWithReuseIdentifier: ImageListCell.CellID)
+    $0.register(
+      ImageListCell.self,
+      forCellWithReuseIdentifier: ImageListCell.CellID
+    )
   }
+  
+  
+  // MARK: - Initialize
+  
+  init(viewModel: ImageListViewModelType = ImageListViewModel()) {
+    self.viewModel = viewModel
+    super.init()
+  }
+  
+  required convenience init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  
+  // MARK: - ViewDidLoad
   
   override func viewDidLoad() {
     super.viewDidLoad()
     self.navigationItem.searchController = self.searchController
   }
   
+  
+  // MARK: - Bind
+  
   override func bind() {
     
+    // INPUT
+    self.searchController.searchBar.rx.text
+      .orEmpty
+      .distinctUntilChanged()
+      .debounce(.seconds(1), scheduler: MainScheduler.instance)
+      .bind(to: self.viewModel.searchImage)
+      .disposed(by: self.disposeBag)
+    
+    // OUTPUT
+    self.viewModel.allImageList
+      .subscribe { imageList in
+        print(imageList)
+      }
+      .disposed(by: self.disposeBag)
   }
   
   override func setupConstraints() {
@@ -54,4 +98,3 @@ class ImageListViewController: BaseViewController {
     }
   }
 }
-

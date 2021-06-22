9dcd7e09f1e8f7ac5158b7c492ec253a85b64b3f
s-doris <swdoriz@gmail.com>
2021-06-22 16:38:57 +0900

    feat : URL을 통한 Image load extension

--- ImageSearch/Sources/Extensions-Utillities/UIImageView+Ext.swift
+++ ImageSearch/Sources/Extensions-Utillities/UIImageView+Ext.swift
@@ -5,4 +5,26 @@
 //  Created by once on 2021/06/22.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+  func setImage(_ url: String) {
+    let key = NSString(string: url)
+    
+    if let cached = ImageCacheManager.shared.object(forKey: key) {
+      self.image = cached
+    } else {
+      guard let url = URL(string: url) else { return }
+      DispatchQueue.global().async { [weak self] in
+        guard let `self` = self else { return }
+        if let data = try? Data(contentsOf: url),
+           let image = UIImage(data: data) {
+          DispatchQueue.main.async {
+            self.image = image
+            ImageCacheManager.shared.setObject(image, forKey: key)
+          }
+        }
+      }
+    }
+  }
+}

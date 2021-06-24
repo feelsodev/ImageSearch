e9ba58202cdd5d9ff94a5d7a393d1147eb2a4007
s-doris <swdoriz@gmail.com>
2021-06-25 00:15:00 +0900

    feat : CollectionView BackgroundView 설정 extesion 선언

--- ImageSearch/Sources/Extensions-Utillities/UICollectionView+Ext.swift
+++ ImageSearch/Sources/Extensions-Utillities/UICollectionView+Ext.swift
@@ -5,4 +5,14 @@
 //  Created by once on 2021/06/24.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionView {
+  func setDescriptView(type: DescriptState) {
+    type.descriptView.frame = CGRect(x: self.center.x,
+                                     y: self.center.y,
+                                     width: self.bounds.size.width,
+                                     height: self.bounds.size.height)
+    self.backgroundView = type.descriptView
+  }
+}

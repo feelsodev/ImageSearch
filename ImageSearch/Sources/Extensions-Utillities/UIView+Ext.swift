09c68f58c7a2a411a13e55b6979b52443050fbc9
s-doris <swdoriz@gmail.com>
2021-06-25 02:37:40 +0900

    feat : UIView FadeIn FadeOut extension init

--- ImageSearch/Sources/Extensions-Utillities/UIView+Ext.swift
+++ ImageSearch/Sources/Extensions-Utillities/UIView+Ext.swift
@@ -5,4 +5,23 @@
 //  Created by once on 2021/06/25.
 //
 
-import Foundation
+import UIKit
+private let durationFadeAnimation: TimeInterval = 0.2
+
+extension UIView {
+  static func animateFadeOut(_ views: [UIView?], completion: (() -> Void)? = nil) {
+    UIView.animate(withDuration: durationFadeAnimation, animations: {
+      views.forEach { $0?.alpha = 0 }
+    }, completion: { _ in
+      completion?()
+    })
+  }
+  
+  static func animateFadeIn(_ views: [UIView?], completion: (() -> Void)? = nil) {
+    UIView.animate(withDuration: durationFadeAnimation, animations: {
+      views.forEach { $0?.alpha = 1 }
+    }, completion: { _ in
+      completion?()
+    })
+  }
+}

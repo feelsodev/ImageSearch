75133b2df22eece38249ce4ef13ed7ec27719961
s-doris <swdoriz@gmail.com>
2021-06-24 19:55:56 +0900

    refactor : ImageListModel 매개변수 관리 viewModel로 위임 및 레거시 함수 제거

---
+++ ImageSearch/Sources/Extensions-Utillities/Reactive+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  Reactive+Ext.swift
+//  ImageSearch
+//
+//  Created by once on 2021/06/24.
+//
+
+import Foundation
--- ImageSearch/Sources/Model/ImageListModel.swift
+++ ImageSearch/Sources/Model/ImageListModel.swift
@@ -8,49 +8,24 @@ import Foundation
 import RxSwift
 
 protocol ImageListFetchable: AnyObject {
-  func propertyReset()
-  func fetchImageList(param: String) -> Observable<[Image]>
-  func nextPageImageList() -> Observable<[Image]>
+  func fetchImageList(page: Int, param: String) -> Observable<Items>
 }
 
 final class ImageListModel: ImageListFetchable {
   let repository: NetworkServiceType
-  private var pageState: Int = 1
-  private var paramState: String = ""
   
   init(repository: NetworkServiceType = NetworkService()) {
     self.repository = repository
   }
   
-  func fetchImageList(param: String) -> Observable<[Image]> {
-    self.paramState = param
-    self.pageState = 1
-    
-    return self.repository.getImageListRx(param: param, page: self.pageState)
+  func fetchImageList(page: Int, param: String) -> Observable<Items> {
+
+    return self.repository.getImageListRx(param: param, page: page)
       .map { data in
         guard let response = try? JSONDecoder().decode(Items.self, from: data) else {
           throw NSError(domain: "Fetch Image Decoding error", code: -1, userInfo: nil)
         }
-        
-        return response.items
+        return response
       }
-  }
-  
-  func nextPageImageList() -> Observable<[Image]> {
-    self.pageState += 1
-
-    return self.repository.getImageListRx(param: self.paramState, page: self.pageState)
-      .map { data in
-        guard let response = try? JSONDecoder().decode(Items.self, from: data) else {
-          throw NSError(domain: "Next page Decoding error", code: -1, userInfo: nil)
-        }
-        
-        return response.items
-      }
-  }
-  
-  func propertyReset() {
-    self.pageState = 1
-    self.paramState = ""
   }
 }

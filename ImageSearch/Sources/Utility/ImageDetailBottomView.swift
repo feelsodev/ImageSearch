7f89c5813cf3fa94de1c8c413da2d9b0bfd51d1c
s-doris <swdoriz@gmail.com>
2021-06-25 01:47:48 +0900

    feat : ImageDetail status view init

--- ImageSearch/Sources/Utility/ImageDetailBottomView.swift
+++ ImageSearch/Sources/Utility/ImageDetailBottomView.swift
@@ -5,4 +5,44 @@
 //  Created by once on 2021/06/25.
 //
 
-import Foundation
+import UIKit
+
+class ImageDetailBottomView: UIView {
+  
+  // MARK: - UI
+  
+  let displaySiteNameLabel = UILabel().then {
+    $0.text = "하하"
+    $0.textColor = .systemBlue
+  }
+  let dateTimeLabel = UILabel().then {
+    $0.text = "하하"
+    $0.textColor = .systemBlue
+  }
+  lazy var stackView = UIStackView().then {
+    $0.axis = .vertical
+    $0.addArrangedSubview(self.displaySiteNameLabel)
+    $0.addArrangedSubview(self.dateTimeLabel)
+  }
+  
+  
+  // MARK: - Initialize
+  
+  init() {
+    super.init(frame: CGRect.zero)
+    self.backgroundColor = .systemGray2
+    self.setupConstraints()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setupConstraints() {
+    self.addSubview(self.stackView)
+    self.stackView.snp.makeConstraints {
+      $0.top.equalToSuperview().offset(5)
+      $0.centerX.equalToSuperview()
+    }
+  }
+}
--- ImageSearch/Sources/Utility/ImageDetailTopView.swift
+++ ImageSearch/Sources/Utility/ImageDetailTopView.swift
@@ -5,4 +5,38 @@
 //  Created by once on 2021/06/25.
 //
 
-import Foundation
+import UIKit
+
+class ImageDetailTopView: UIView {
+  
+  // MARK: - UI
+  
+  let backButton = UIButton().then {
+    $0.tintColor = .systemBlue
+    $0.setImage(UIImage().setSFSymbols(systemName: "xmark",
+                                       weight: .regular), for: .normal)
+  }
+  
+  
+  // MARK: - Initialize
+  
+  init() {
+    super.init(frame: CGRect.zero)
+    self.backgroundColor = .systemGray2
+    self.setupConstraints()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setupConstraints() {
+    self.addSubview(self.backButton)
+    
+    self.backButton.snp.makeConstraints {
+      $0.leading.equalToSuperview().offset(10)
+      $0.bottom.equalToSuperview().offset(-10)
+      $0.width.height.equalTo(20)
+    }
+  }
+}

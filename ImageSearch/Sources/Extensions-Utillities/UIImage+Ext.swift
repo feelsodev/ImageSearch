e60b81cde5694114ba67c2f08633c01167e9b455
s-doris <swdoriz@gmail.com>
2021-06-25 00:13:55 +0900

    feat : UIImage SFSymbol extension 선언

--- ImageSearch/Sources/Extensions-Utillities/UIImage+Ext.swift
+++ ImageSearch/Sources/Extensions-Utillities/UIImage+Ext.swift
@@ -5,4 +5,12 @@
 //  Created by once on 2021/06/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+  func setSFSymbols(systemName: String, weight: SymbolWeight) -> UIImage {
+    guard let image = UIImage(systemName: systemName)?
+            .withConfiguration(UIImage.SymbolConfiguration(weight: weight)) else { return UIImage() }
+    return image
+  }
+}

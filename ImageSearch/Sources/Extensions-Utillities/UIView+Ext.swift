042e87d15623fe51b2f6fb6712b97a886edd0448
s-doris <swdoriz@gmail.com>
2021-06-25 02:32:58 +0900

    feat : ImageDetail 상태바 fadeIn fadeOut 상태값 핸들링

---
+++ ImageSearch/Sources/Extensions-Utillities/UIView+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Ext.swift
+//  ImageSearch
+//
+//  Created by once on 2021/06/25.
+//
+
+import Foundation
--- ImageSearch/Sources/ViewController/ImageDetailViewController.swift
+++ ImageSearch/Sources/ViewController/ImageDetailViewController.swift
@@ -6,20 +6,58 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxGesture
 
 final class ImageDetailViewController: BaseViewController {
+  
+  // MARK: - Property
+  
+  var statusState: Bool = true
+  
+  
+  // MARK: - UI
+  
   let detailImageView = UIImageView()
   let scrollView = UIScrollView()
   let topStatusView = ImageDetailTopView()
   let bottomStatusView = ImageDetailBottomView()
   
+  
+  // MARK: - Status Bar handle variable
+  
+  override var prefersStatusBarHidden: Bool {
+    return !self.statusState
+  }
+  override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
+    return .fade
+  }
+  
+  
+  // MARK: - ViewDidLoad
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    self.view.backgroundColor = .black
+    self.view.backgroundColor = .white
     self.scrollView.do {
       $0.contentSize = self.detailImageView.bounds.size
     }
   }
+  
+  
+  // MARK: - Bind
+  
+  override func bind() {
+    self.view.rx.tapGesture()
+      .when(.recognized)
+      .map { _ in }
+      .subscribe(onNext: self.statusBarHandle)
+      .disposed(by: self.disposeBag)
+  }
+  
+  
+  // MARK: - SetupConstraints
   
   override func setupConstraints() {
     [self.topStatusView, self.bottomStatusView, self.scrollView]
@@ -42,6 +80,23 @@ final class ImageDetailViewController: BaseViewController {
     self.detailImageView.snp.makeConstraints {
       $0.leading.trailing.equalTo(self.scrollView.frameLayoutGuide)
       $0.centerY.equalTo(self.scrollView)
+    }
+  }
+}
+
+extension ImageDetailViewController {
+  private func statusBarHandle() {
+    self.statusState.toggle()
+    if self.statusState {
+      UIView.animateFadeIn([self.topStatusView, self.bottomStatusView])
+      UIView.animate(withDuration: 0.2, animations: {
+        self.setNeedsStatusBarAppearanceUpdate()
+      })
+    } else {
+      UIView.animateFadeOut([self.topStatusView, self.bottomStatusView])
+      UIView.animate(withDuration: 0.2, animations: {
+        self.setNeedsStatusBarAppearanceUpdate()
+      })
     }
   }
 }
